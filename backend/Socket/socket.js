001aaa482464785bb0af651072663d5cfc019faa
vid123ver <154465359+vid123ver@users.noreply.github.com>
2025-12-28 10:09:04 +0530

    Enhance CORS and user socket management
    Updated CORS configuration to allow requests from a new production URL and improved user socket management logic.

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -1,33 +1,40 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-import cors from "cors";
 
 const app = express();
-
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://real-time-chat-platform.onrender.com"
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
+// Map to store online users
+const userSocketmap = {};
+
 export const getReceiverSocketId = (receiverId) => {
   return userSocketmap[receiverId];
 };
 
-const userSocketmap = {};
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
 
-  if (userId !== "undefined") userSocketmap[userId] = socket.id;
+  if (userId && userId !== "undefined") {
+    userSocketmap[userId] = socket.id;
+  }
+
   io.emit("getOnlineUser", Object.keys(userSocketmap));
 
   socket.on("disconnect", () => {
     delete userSocketmap[userId];
-      io.emit("getOnlineUser", Object.keys(userSocketmap));
+    io.emit("getOnlineUser", Object.keys(userSocketmap));
   });
 });
 
